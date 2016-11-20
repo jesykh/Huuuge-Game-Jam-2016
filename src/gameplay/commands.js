9555f611a033d4b3959a468197986a8e253fb815
Zosia Sobocinska <Zosia Sobocinska>
2016-11-20 09:54:08 +0100

    Let the snake be controlled with keyboard
    Voice controls would be supported too but for the API not supported
    by ElectronJS

---
+++ src/gameplay/commands.js
@@ -0,0 +1,36 @@
+import { Status } from './status';
+
+export var Commands = {
+  _commands: [],
+  addCommand: function(commandName) {
+    this._commands.push(commandName);
+  },
+  popCommand: function() {
+    return this._commands.shift();
+  },
+  hasCommands: function() {
+    return this._commands.length > 0;
+  }
+}
+
+var KEY_BINDINGS = {
+  ArrowLeft: 'left',
+  ArrowUp: 'up',
+  ArrowRight: 'right',
+  ArrowDown: 'down'
+}
+
+$('body').keydown(function(event) {
+  var command = KEY_BINDINGS[event.key];
+  if (command) {
+    Commands.addCommand(command);
+  }
+});
+
+annyang.addCommands({
+  left: function() {Commands.addCommand('left')},
+  up: function() {Commands.addCommand('up')},
+  right: function() {Commands.addCommand('right')},
+  down: function() {Commands.addCommand('down')},
+});
+annyang.start();
